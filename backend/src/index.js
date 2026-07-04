3d4e82830462d31b1098e206afbdc4768dd64e4d
MD Samiul Islam <isamiul099@gmail.com>
2026-07-04 13:13:05 +0530

    removed timestamp in /health endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,8 +11,7 @@ const app = express();
 
 app.get('/health', (req,res) => {
     res.status(200).json({
-        message : "Server is healthy",
-        timestamp : new Date.now
+        message : "Server is healthy"
     })
 })
 
